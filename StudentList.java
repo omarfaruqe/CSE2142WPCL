195d8ec83a1e2d1f3ef71b003b8c2002e624df05
Rejaul Karim Rana <rana.cse333@gmail.com>
2019-07-26 00:21:20 +0600

    Adds handling completed

--- StudentList.java
+++ StudentList.java
@@ -118,6 +118,9 @@ public class StudentList {
 		System.out.println("Data Loaded.");	
 		
 		}
+		
+		else 
+			System.out.println("Give proper input.");
 	}
 	
 	public static BufferedReader bufferedReader(){
